fd7c48d6695142620aa67cf675d420424d92f2b9
TheDog <TheDogKSP@users.noreply.github.com>
2016-04-14 10:49:35 +0200

    Update StateFundingScenario.cs

--- Instance/StateFundingScenario.cs
+++ Instance/StateFundingScenario.cs
@@ -2,11 +2,41 @@
 
 namespace StateFunding
 {
-  public class StateFundingScenario
+  public class StateFundingScenario : ScenarioModule
   {
-    public StateFundingScenario ()
-    {
+    private static StateFundingScenario _instance;
+    public StateFundingScenario Instance {
+      get {
+        return _instance;
+      }
     }
+    
+    private static InstanceData data;
+    private static bool isInit;
+    
+    public StateFundingScenario () {
+      if (_instance != null)
+        return;
+        
+      _instance = this;
+      data = new InstanceData();
+    }
+    
+    
+    //load scenario
+    public override void OnLoad (ConfigNode node) {
+      
+    }
+    
+    
+    //save scenario
+    public override void OnSave (ConfigNode node) {
+      if (!isInit)
+        return;
+      
+    }
+    
+    
   }
 }
 
