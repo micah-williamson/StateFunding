4793096ed27d14cdac057fd72d49a9d0bfcc2567
TheDog <TheDogKSP@users.noreply.github.com>
2016-04-14 10:22:27 +0200

    Update OnSpaceCentre.cs
    integrate update/tick from everyscenes

--- Hooks/OnSpaceCentre.cs
+++ Hooks/OnSpaceCentre.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine;
 
 namespace StateFunding {
-  [KSPAddon (KSPAddon.Startup.SpaceCentre, false)]
+  [KSPAddon (KSPAddon.Startup.SpaceCentre, true)]
   public class OnSpaceCentre : MonoBehaviour {
     public void Awake () {
       
@@ -14,8 +14,19 @@ namespace StateFunding {
       StateFundingGlobal.fetch.LoadIfNeeded ();
     }
 
+
+    private int curTicks;
+    private const int INTERVAL_TICKS = 50;
+
     public void Update () {
-      
+      // Update once every 50 fixedupdates
+      curTicks++;
+      if (curTicks > INTERVAL_TICKS) {
+        curTicks = 0;
+        if (StateFundingGlobal.fetch != null) {
+          StateFundingGlobal.fetch.tick ();
+        }
+      }
     }
 
     public void FixedUpdate () {
